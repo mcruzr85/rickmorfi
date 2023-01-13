48a6318c2795d92906da961aa24481748ce71a9f
mcruzr85 <mcruzrodriguez1985@gmail.com>
2023-01-13 01:07:43 -0300

    favorite

---
+++ src/components/Favorite/Favorite.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import style from "./Favorite.module.css";
+
+const Favorite = ({ id, name, image, species, gender }) => {
+  
+  return (
+    <div className={style.card}>
+      <div>
+        <img className={style.foto} src={image} alt="una imagen" />
+      </div>
+
+      <div className={`${style.info} ${style.text}`}>
+       <div><Link className={style.link} to={`/detail/${id}`}>
+          <h2>{`- ${id} -`}</h2>
+        </Link>
+        <h2>❤️</h2>      
+          </div>
+       
+        {(name.length < 21) && <h2>{`Name: ${name}`}</h2>}
+        {(name.length >= 21) && <div><h2>{`Name:`}</h2> <h2>{`${name}`}</h2></div>}
+        <h2>{`Gender: ${gender}`}</h2>
+        {(species.length < 21) && <h2>{`Species: ${species}`}</h2>}
+        {(species.length >= 21) && <div><h2>{`Species:`}</h2> <h2>{`${species}`}</h2></div>}
+      </div>
+    </div>
+  );
+};
+
+export default Favorite;
---
+++ src/components/Favorites/Favorites.jsx
@@ -0,0 +1,64 @@
+import { connect } from "react-redux";
+//import Card from "../Card/Card";
+import Favorite from "../Favorite/Favorite";
+import style from './Favorites.module.css'
+
+function Favorites({ myFavorites }) {
+
+  return (
+    <div  className={style.cardsContainer}>
+      {myFavorites?.map((f) => {
+        
+        console.log(f)
+        
+       return (        
+          <Favorite key={f.id} id={f.id} name={f.name} image={f.image} species={f.species}
+          gender={f.gender} />
+        )
+      })}
+    </div>
+  );
+}
+
+
+function mapStateToProps(state) {
+  return {
+    myFavorites: state.myFavorites, //le pasa el estado global al componente
+  };
+}
+
+export default connect(mapStateToProps)(Favorites);
+
+/*export default function Favorites(props) {
+  //mapeo de favoritos
+
+  const myFavorites = useSelector((state) => state.myFavorites);
+  return (
+    <div>
+      {myFavorites.map((f)=>(
+      <Card
+        key={f.id}
+        id={f.id}
+        name={f.name}
+        species={f.species}
+        gender={f.gender}
+        image={f.image}       
+      />
+      ) )}
+    </div>
+  );
+}*/
+
+/**original
+ * return(
+<div>
+  <h1>Favorites</h1>
+   {myFavorites?.map(ch => (
+    <div>
+    <h3>{ch.name}</h3>
+    <img src={ch.image} alt="" />
+    </div>
+   ))}
+</div>
+);
+ */
